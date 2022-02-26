e54101d9963f4c1f05a6f5ce396f04a678d3af53
vj <com.vjc@gmail.com>
2022-02-26 21:24:57 +0530

    added API

---
+++ MoviesDB/API.swift
@@ -0,0 +1,32 @@
+//
+//  URLs.swift
+//  MoviesDB
+//
+//  Created by Vijaychandran Jayachandran on 26/2/22.
+//
+
+import Foundation
+
+struct API {
+    private static let baseURL = "http://www.omdbapi.com"
+    
+    private static let apiKey = "b9bd48a6"
+    
+    private static let defaultSearchText = "Marvel"
+    private static let defaultType = "movie"
+    
+    static func fetchMoviesURL(searchText s: String = defaultSearchText) -> URL {
+        guard let url = URL(string: "\(baseURL)/?apikey=\(apiKey)&s=\(s)&type=\(defaultType)") else {
+            fatalError("invalid MoviesURL")
+        }
+        return url
+    }
+    
+    static func fetchMoviesDetails(id: String) -> URL {
+        guard let url = URL(string: "\(baseURL)/?apikey=\(apiKey)&i=\(id))") else {
+            fatalError("invalid MoviesURL")
+        }
+        return url
+    }
+
+}
