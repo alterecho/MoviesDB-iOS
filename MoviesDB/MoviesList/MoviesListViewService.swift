2e1f81be047e3cede63b473705afce4f28c9bc3f
vj <com.vjc@gmail.com>
2022-02-27 11:15:27 +0530

    added movies list service

--- MoviesDB/MoviesList/MoviesListViewService.swift
+++ MoviesDB/MoviesList/MoviesListViewService.swift
@@ -5,4 +5,14 @@
 //  Created by Vijaychandran Jayachandran on 27/2/22.
 //
 
+import Combine
 import Foundation
+
+class MoviesListViewService {
+    func search(_ searchString: String) -> AnyPublisher<[Movie], Error> {
+        let request = URLRequest(url: API.fetchMoviesURL(searchText: searchString))
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map { print($0.data.count); return $0.data }.decode(type: [Movie].self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
