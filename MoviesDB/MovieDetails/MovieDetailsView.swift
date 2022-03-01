480f0d2052dd8f202d7ec00ca2be89472a85cbab
vj <com.vjc@gmail.com>
2022-03-01 22:38:53 +0530

    details view basic styling

--- MoviesDB/MovieDetails/MovieDetailsView.swift
+++ MoviesDB/MovieDetails/MovieDetailsView.swift
@@ -14,21 +14,23 @@ struct MovieDetailsView<ViewModel: MovieDetailsViewModelProtocol>: View {
     
     var body: some View {
          NavigationView {
-             ZStack {
-                 VStack {
-                     getPosterTitleView()
-                     getSubDetailsView()
-                     getSynopsisView()
-                     getMetricsView()
-                     getCreditsView()
-                 }
-                 if viewModel.isLoading {
-                     ProgressView().frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.black.opacity(0.25))
-                 }
+             ScrollView {
+                 ZStack {
+                     VStack(spacing: 20.0) {
+                         getPosterTitleView()
+                         getSubDetailsView()
+                         getSynopsisView()
+                         getMetricsView()
+                         getCreditsView()
+                     }
+                     if viewModel.isLoading {
+                         ProgressView().frame(maxWidth: .infinity, maxHeight: .infinity).background(Color.black.opacity(0.25))
+                     }
+                 }.padding()
+             }.navigationTitle(viewModel.pageTitle).onAppear {
+                 viewModel.onAppear()
              }
-        }.navigationTitle(viewModel.pageTitle).onAppear {
-            viewModel.onAppear()
-        }.alert(isPresented: $viewModel.alert.isShowing) {
+        } .alert(isPresented: $viewModel.alert.isShowing) {
             Alert(title: Text(viewModel.alert.title ?? ""), message: Text(viewModel.alert.message ?? ""),
                   dismissButton: .default(Text(viewModel.alert.buttonTitle ?? "")) {
                 
@@ -37,38 +39,38 @@ struct MovieDetailsView<ViewModel: MovieDetailsViewModelProtocol>: View {
     }
  
     func getPosterTitleView() -> some View {
-        ZStack {
+        ZStack(alignment: .bottom) {
+            let screenSize = UIScreen.main.bounds.size
             if let image = viewModel.poster {
-                Image(uiImage: image)
+                Image(uiImage: image).resizable().frame(maxWidth: .infinity, maxHeight: screenSize.height / 2.0).background(Color.black).scaledToFit()
             }
             
             HStack {
-                VStack {
-                    Text(viewModel.details?.title ?? "")
-                    Text(viewModel.details?.year ?? "")
-                }
-            }
-            
-        }
+                VStack(alignment: .leading) {
+                    Text(viewModel.details?.title ?? "").foregroundColor(.white).fontWeight(.heavy).frame(maxWidth: .infinity, alignment: .leading)
+                    Text(viewModel.details?.year ?? "").foregroundColor(.white).fontWeight(.light)
+                }//.frame(maxWidth: .infinity).background(Color.black.opacity(0.5))
+            }.padding()
+        }.background(Color.black)
     }
     
     func getSubDetailsView() -> some View {
-        HStack {
+        HStack(spacing: 20.0) {
             Text(viewModel.details?.categories ?? "")
             Text(viewModel.details?.runtime ?? "")
             Text(viewModel.details?.ratings ?? "")
-        }
+        }.frame(maxWidth: .infinity).font(.callout)
     }
     
     func getSynopsisView() -> some View {
         VStack {
-            Text(viewModel.synopsisTitle)
-            Text(viewModel.details?.synopsis ?? "")
+            Text(viewModel.synopsisTitle).font(.title).fontWeight(.bold).frame(maxWidth: .infinity, alignment: .leading)
+            Text(viewModel.details?.synopsis ?? "").font(.body)
         }
     }
     
     func getMetricsView() -> some View {
-        HStack {
+        HStack(spacing: 20.0) {
             VerticalValueView(title: viewModel.scoreTitle, value: viewModel.details?.score)
             VerticalValueView(title: viewModel.reviewTitle, value: viewModel.details?.reviews)
             VerticalValueView(title: viewModel.popularityTitle, value: viewModel.details?.popularity)
@@ -92,10 +94,10 @@ struct VerticalValueView: View {
     var body: some View {
         VStack {
             if let title = title {
-                Text(title)
+                Text(title).font(.subheadline)
             }
             if let value = value {
-                Text(value)
+                Text(value).font(.title).fontWeight(.bold)
             }
         }
     }
@@ -106,14 +108,20 @@ struct HorizontalValueView: View {
     let value: String?
     
     var body: some View {
-        HStack {
+        HStack(alignment:.top, spacing: 20.0) {
             if let title = title {
-                Text(title)
+                Text(title).font(.body)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
             }
             if let value = value {
-                Text(value)
+                Text(value).font(.headline).frame(maxWidth: .infinity, alignment: .leading)
             }
         }
     }
 }
 
+struct MovieDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieDetailsView(viewModel: MovieDetailsViewModel(imdbID: "tt4154664")).environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
